c3ab586a6b74a8cadec1501d9ac40f8470f263c1
moe booka <81874361+moemnml33@users.noreply.github.com>
2024-10-02 18:37:51 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    let member = document.getElementById("member");
+    let memberInfo = document.getElementById("memberInfo");
+    let memberSkills = document.getElementById("memberSkills");
+
+    memberInfo.style.display = "none";
+    memberSkills.style.display = "block";
+    member.style.backgroundColor = "white";
+    member.style.color = "black";
+}
